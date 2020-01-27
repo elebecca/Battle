187eae5d16268b2434e217cd4195858dd44cdfcf
hansa-lallu <hansalallu@gmail.com>
2020-01-27 15:57:49 +0000

    completed step 5

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -5,4 +5,7 @@ class Oystercard
     @balance = 0
   end
 
+  def top_up(value)
+    balance += value 
+  end 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,6 +2,10 @@ describe Oystercard do
   it 'initializes with a default balance of 0' do
     expect(subject.balance).to eq 0
   end
+
+  it 'adds money to the card' do
+    expect(subject.top_up(30)).to eq 30
+  end
 end 
 
 
