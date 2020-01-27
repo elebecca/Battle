3ee156d3d7893012ba645958c6d09bcc12172cdd
Jason <jasylwong@gmail.com>
2020-01-27 15:40:38 +0000

    Challenge 4 added

--- spec/oyster_spec.rb
+++
@@ -1,7 +0,0 @@
-# require 'oyster'
-
-describe Oyster do
-  # it {is_expected.to repsond_to :touch}
-end 
-
-
---
+++ spec/oystercard_spec.rb
@@ -0,0 +1,7 @@
+describe Oystercard do
+  it 'initializes with a default balance of 0' do
+    expect(subject.balance).to eq 0
+  end
+end 
+
+
